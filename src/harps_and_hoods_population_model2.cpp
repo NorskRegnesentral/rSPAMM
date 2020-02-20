1420b1a0744158b0a31d0769b09d2ad799b1b357
oigard <tor.arne.oigard@nr.no>
2020-02-20 10:48:19 +0100

    Small change in cpp.

--- src/harps_and_hoods_population_model2.cpp
+++ src/harps_and_hoods_population_model2.cpp
@@ -241,7 +241,7 @@ Type objective_function<Type>::operator() ()
  
  NTotmax = max(NTot);
  
- DNmax = NTotpred/NTotmax;
+ DNmax = NTotPred/NTotmax;
  
  //NTotmax = NTot(CppAD::Integer(maxind));
  //DNmax /= NTot(CppAD::Integer(maxind));
